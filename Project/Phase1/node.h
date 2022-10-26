75c1d7945d812cfbb50731f1cc4cef2b9ae22e96
Ljfanny <84827635+Ljfanny@users.noreply.github.com>
2022-10-26 16:51:33 +0800

    add all files.
    Need to add error and CHAR type judgement.

---
+++ Project/Phase1/node.h
@@ -0,0 +1,25 @@
+#ifndef _NODE_TYPE
+#define _NODE_TYPE
+
+struct node {
+    int type;
+    int line;
+    char *value;
+    int number;
+    struct node *children[10];
+};
+
+void setNode(struct node *prt, struct node *nds[], int number);
+
+struct node *newNodeNL(int type, int line, char *content);
+
+struct node *newNodeTER(int type, int line);
+
+struct node *newNodeNTER(int type, int line);
+
+void treePrint(struct node* nd);
+
+void nodePrint(struct node *nd, int space);
+
+#endif
+
