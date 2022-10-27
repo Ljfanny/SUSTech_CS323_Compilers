823eb7588ca987bc59f5593fa747c871aabbbe37
Yifei <3335949629@qq.com>
2022-10-28 01:21:04 +0800

    test

---
+++ Project/Phase1/make.py
@@ -0,0 +1,10 @@
+import os
+
+for i in range(5):
+
+    file = open('./test/test_12011906_'+str(i+1)+'.spl', 'w')
+    file.close()
+    file = open('./test/test_12011543_'+str(i+1)+'.spl', 'w')
+    file.close()
+    file = open('./test/test_11911212_'+str(i+1)+'.spl', 'w')
+    file.close()
