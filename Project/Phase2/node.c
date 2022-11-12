6ceb01c4ede9028f67b1f8efbfb664e9845c209f
Ljfanny <84827635+Ljfanny@users.noreply.github.com>
2022-11-12 20:16:34 +0800

    Update Phase2
    Already be back to basics.

---
+++ Project/Phase2/node.c
@@ -0,0 +1,83 @@
+#include "node.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+// #include "syntax.tab.c"
+int nter_idx = 46;
+char *NDtypes[] = {"TYPE", "INT", "FLOAT", "CHAR", "ID","STRUCT", "IF", "WHILE", "ELSE", "RETURN",
+        "EXTERN", "STATIC", "CONST", "QUESTION_MARK", "COLON",
+        "DOT",  "SEMI", "COMMA", "ASSIGN", "PLUS_ASSIGN", "MINUS_ASSIGN",
+        "MUL_ASSIGN", "DIV_ASSIGN", "MOD_ASSIGN",
+        "LT", "LE", "GT", "GE", "NE", "EQ", "AND",
+        "OR", "NOT", "PLUS", "MINUS","DOUBLE_PLUS","DOUBLE_MINUS", "MUL",
+        "DIV", "MOD", "LP", "RP", "LB", "RB",
+        "LC", "RC","Program", "ExtDefList",
+        "ExtDef", "Specifier", "ExtDecList", "StructSpecifier",
+        "VarDec", "FunDec", "VarList", "ParamDec",
+        "CompSt", "StmtList", "Stmt", "DefList",
+        "Def", "DecList", "Dec", "Args", "Exp"
+};
+
+void setNode(struct node *prt, struct node *nds[], int number) {
+    prt->number = number;
+    int min = 2147483647;
+    for (int i = 0; i < number; i++) {
+        prt->children[i] = nds[i];
+        if(nds[i] != NULL && nds[i]->line > 0){
+            min = min < nds[i]->line ? min : nds[i]->line;
+        }
+    }
+    prt->line = min < prt->line?min:prt->line;
+}
+
+struct node *newNodeNL(int type, int line, char *content) {
+    struct node *nd = (struct node *) malloc(sizeof(struct node));
+    nd->line = line;
+    nd->type = type;
+    nd->number = 0;
+    nd->value = (char *) malloc(sizeof(char) * strlen(content));
+    strcpy(nd->value, content);
+    return nd;
+}
+
+struct node *newNodeTER(int type, int line) {
+    struct node *nd = (struct node *) malloc(sizeof(struct node));
+    nd->line = line;
+    nd->type = type;
+    nd->number = 0;
+    nd->value = NULL;
+    return nd;
+}
+
+struct node *newNodeNTER(int type, int line) {
+    struct node *nd = (struct node *) malloc(sizeof(struct node));
+    nd->line = line;
+    nd->type = type;
+    nd->number = 0;
+    nd->value = NULL;
+    return nd;
+}
+
+void nodePrint(struct node *nd, int space) {
+    if (nd == NULL)
+        return;
+    for (int i = 0; i < space; ++i)
+        printf(" ");
+    if (nd->value == NULL) {
+        if (nd->type >= nter_idx) {
+            printf("%s (%d)\n", NDtypes[nd->type], nd->line);
+        } else if(nd->type >= 0) {
+            printf("%s\n", NDtypes[nd->type]);
+        }
+    }else {
+        printf("%s: %s\n", NDtypes[nd->type], nd->value);
+    }
+    for (int i = 0; i < nd->number; ++i) {
+        nodePrint(nd->children[i], space + 2);
+    }
+}
+
+void treePrint(struct node* nd) {
+    nodePrint(nd, 0);
+}
+
